7c20dd203fff111b3bdd18c8b1c22616c4dffb32
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-09-19 17:36:49 -0700

    Added styling to css and refined questions and answers in js

--- js/app.js
+++ js/app.js
@@ -1,68 +1,67 @@
-'use strict';
-
-/* Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical. Or not. It's up to you! It's your project!
-The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
-Useful and descriptive console.log() messages in the JS are well written and correctly displaying to the browser console for each question of the guessing game.
-Use CSS to style your page as you see fit, to the level of abilities in CSS that you currently have
-Add, Commit, Push process is being followed; it is evident in GitHub that commits are made regularly, and with good commit messages that explain the WHY of the commit. */
-
-// var userName = prompt('What is your name?');
+// 'use strict';
 //
-// alert('Hello ' + userName);
-
-// Question 1
-
-var answer1 = prompt('Do I live in Colorado?').toLowerCase();
-console.log('Answer 1 is, ' + answer1 + '.')
-
-// var answer2 = prompt('Do I live in Colorado?').toLowerCase();
-// console.log('Answer 1 is, ' + answer1 + '.')
+// /* Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical. Or not. It's up to you! It's your project!
+// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toUpperCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+// Useful and descriptive console.log() messages in the JS are well written and correctly displaying to the browser console for each question of the guessing game.
+// Use CSS to style your page as you see fit, to the level of abilities in CSS that you currently have
+// Add, Commit, Push process is being followed; it is evident in GitHub that commits are made regularly, and with good commit messages that explain the WHY of the commit. */
 //
-// var answer3 = prompt('Do I live in Colorado?').toLowerCase();
-// console.log('Answer 1 is, ' + answer1 + '.')
-//
-// var answer4 = prompt('Do I live in Colorado?').toLowerCase();
-// console.log('Answer 1 is, ' + answer1 + '.')
-//
-// var answer5 = prompt('Do I live in Colorado?').toLowerCase();
-// console.log('Answer 1 is, ' + answer1 + '.')
+var userName = prompt('What is your name?');
 
-if (answer1 === 'n' || answer1 === 'no') {
+alert('Hello, ' + userName + ' nice to meet you!');
+
+// Did I live in Colorado?
+
+var currentStateResidence = prompt('Do I live in Colorado?').toUpperCase();
+console.log('Response to currentStateResidence is: ' + currentStateResidence)
+
+if (currentStateResidence === 'N' || currentStateResidence === 'NO') {
   //Tell user they got the correct answer.
-  alert('You are correct.');
+  alert('That\'s right, I live in Seattle!');
 } else {
   //Tell user they got the wrong answer.
-  alert('You are incorrect')
+  alert('Nope, but I would live there again in a heartbeat!');
   }
-//
-// if (answer2 === 'n' || answer2 === 'no') {
-//   //Tell user they got the correct answer.
-//   alert('You are correct.');
-// } else {
-//   //Tell user they got the wrong answer.
-//   alert('You are incorrect')
-//   }
-//
-// if (answer3 === 'n' || answer3 === 'no') {
-//   //Tell user they got the correct answer.
-//   alert('You are correct.');
-// } else {
-//   //Tell user they got the wrong answer.
-//   alert('You are incorrect')
-//   }
-//
-// if (answer4 === 'n' || answer4 === 'no') {
-//   //Tell user they got the correct answer.
-//   alert('You are correct.');
-// } else {
-//   //Tell user they got the wrong answer.
-//   alert('You are incorrect')
-//   }
-//
-// if (answer5 === 'n' || answer5 === 'no') {
-//   //Tell user they got the correct answer.
-//   alert('You are correct.');
-// } else {
-//   //Tell user they got the wrong answer.
-//   alert('You are incorrect')
-//   }
+
+var highestElevationRidden = prompt('Have I ridden my bike past the equivalent of two miles in elevation?').toUpperCase();
+console.log('Response to highestElevationRidden is: ' + highestElevationRidden + '.')
+
+if (highestElevationRidden === 'Y' || highestElevationRidden === 'YES') {
+  //Tell user they got the correct answer.
+  alert('Soooo tough to breathe.');
+} else {
+  //Tell user they got the wrong answer.
+  alert('Well, technically I pushed my bike past that elevation. ;-) ');
+  }
+
+var foreignCountryVisited = prompt('Have I ever been to Canada?').toUpperCase();
+console.log('Response to foreignCountryVisited is: ' + foreignCountryVisited)
+
+if (foreignCountryVisited === 'Y' || foreignCountryVisited === 'YES') {
+  //Tell user they got the correct answer.
+  alert('Yep!  I\'ve been to Victoria and Vancouver.');
+} else {
+  alert('I\'ve actually been a few times.');
+  }
+
+var firstCar = prompt('Was my first car a Ford?').toUpperCase();
+console.log('Response to firstCar is: ' + firstCar)
+
+if (firstCar === 'N' || firstCar === 'NO') {
+  //Tell user they got the correct answer.
+  alert('That\'s right!  It was a Honda Civic with 220k miles!');
+} else {
+  //Tell user they got the wrong answer.
+  alert('Nope, but the new Ford Focus hatchbacks are pretty rad!');
+  }
+
+var futureCityResidence = prompt('Will I move to Los Angeles in the next three years?').toUpperCase();
+console.log('Response to futureCityResidence is: ' + futureCityResidence)
+
+if (futureCityResidence === 'Y' || futureCityResidence === 'YES') {
+  //Tell user they got the correct answer.
+  alert('Fingers crossed.');
+} else {
+  //Tell user they got the wrong answer.
+  alert('Yeah, you\'re probably right.  I should stick around the PNW.');
+  }
